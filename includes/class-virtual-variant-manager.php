ae52d2485fa46192df2ed1b3d3094fcb19420091
giftformekft-tech <giftformekft@gmail.com>
2026-01-22 00:45:06 +0100

    Remove global attribute selector UI

--- includes/class-virtual-variant-manager.php
+++ includes/class-virtual-variant-manager.php
@@ -35,6 +35,14 @@ class MG_Virtual_Variant_Manager {
         return $product->is_type('simple');
     }
 
+    public static function has_virtual_variants($product) {
+        if (!self::is_supported_product($product)) {
+            return false;
+        }
+        $config = self::get_frontend_config($product);
+        return !empty($config) && !empty($config['types']);
+    }
+
     public static function enqueue_assets() {
         if (!function_exists('is_product') || !is_product()) {
             return;
@@ -74,15 +82,12 @@ class MG_Virtual_Variant_Manager {
             true
         );
 
-        $config['ajax'] = array(
-            'url' => admin_url('admin-ajax.php'),
-            'nonce' => wp_create_nonce(self::NONCE_ACTION),
-        );
         $config['product'] = array(
             'id' => $product->get_id(),
             'design_id' => self::get_design_id($product),
             'render_version' => self::get_render_version($product),
         );
+        $config['render_base_url'] = self::get_render_base_url();
 
         wp_localize_script('mg-virtual-variant-display', 'MG_VIRTUAL_VARIANTS', $config);
     }
