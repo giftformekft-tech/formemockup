bd8ec07975bd1218bc915277cbc581f8154c5ead
giftformekft-tech <giftformekft@gmail.com>
2026-01-22 01:04:29 +0100

    Match frontend mockup filenames to bulk outputs

--- includes/class-virtual-variant-manager.php
+++ includes/class-virtual-variant-manager.php
@@ -35,6 +35,14 @@ class MG_Virtual_Variant_Manager {
         return $product->is_type('simple');
     }
 
+    public static function has_virtual_variants($product) {
+        if (!self::is_supported_product($product)) {
+            return false;
+        }
+        $config = self::get_frontend_config($product);
+        return !empty($config) && !empty($config['types']);
+    }
+
     public static function enqueue_assets() {
         if (!function_exists('is_product') || !is_product()) {
             return;
@@ -74,15 +82,12 @@ class MG_Virtual_Variant_Manager {
             true
         );
 
-        $config['ajax'] = array(
-            'url' => admin_url('admin-ajax.php'),
-            'nonce' => wp_create_nonce(self::NONCE_ACTION),
-        );
         $config['product'] = array(
             'id' => $product->get_id(),
             'design_id' => self::get_design_id($product),
             'render_version' => self::get_render_version($product),
         );
+        $config['render_base_url'] = self::get_render_base_url();
 
         wp_localize_script('mg-virtual-variant-display', 'MG_VIRTUAL_VARIANTS', $config);
     }
@@ -310,6 +315,11 @@ class MG_Virtual_Variant_Manager {
         $preview_preload_default = $total_colors < 80;
         $preview_preload = (bool) apply_filters('mg_virtual_variant_preview_preload', $preview_preload_default, $product, $types_payload, $total_colors);
 
+        $primary_view = apply_filters('mg_virtual_variant_primary_view', 'front', $product, $types_payload);
+        $primary_view = sanitize_title($primary_view);
+        if ($primary_view === '') {
+            $primary_view = 'front';
+        }
         $config = array(
             'types' => $types_payload,
             'order' => array(
@@ -318,6 +328,7 @@ class MG_Virtual_Variant_Manager {
             'typeUrls' => $type_urls,
             'preview_cache_limit' => $preview_cache_limit,
             'preview_preload' => $preview_preload,
+            'primary_view' => $primary_view,
             'priceFormat' => array(
                 'currencySymbol' => function_exists('get_woocommerce_currency_symbol') ? get_woocommerce_currency_symbol() : '',
                 'priceFormat' => function_exists('get_woocommerce_price_format') ? get_woocommerce_price_format() : '%1$s%2$s',
