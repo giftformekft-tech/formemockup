99020d8900ff6e73ec28397ad25ef88e844b9431
giftformekft-tech <giftformekft@gmail.com>
2026-02-17 18:10:23 +0100

    asd

--- astra-child/functions.php
+++ astra-child/functions.php
@@ -27,6 +27,108 @@ function astra_child_enqueue_styles() {
 add_action('wp_enqueue_scripts', 'astra_child_enqueue_styles', 15);
 
 /**
+ * Override WooCommerce price display for variant products
+ */
+function astra_child_custom_price_display() {
+    // Only on single product pages with mg_type parameter
+    if (!is_product() || !isset($_GET['mg_type'])) {
+        return;
+    }
+    
+    if (!class_exists('MG_Virtual_Variant_Manager')) {
+        return;
+    }
+    
+    global $product;
+    if (!$product) {
+        return;
+    }
+    
+    $requested_type = sanitize_text_field($_GET['mg_type']);
+    $config = MG_Virtual_Variant_Manager::get_frontend_config($product);
+    
+    if (empty($config) || empty($config['types']) || !isset($config['types'][$requested_type])) {
+        return;
+    }
+    
+    // Remove default WooCommerce price hooks
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+    
+    // Add our custom price display at the same position
+    add_action('woocommerce_single_product_summary', 'astra_child_display_variant_price', 10);
+}
+add_action('woocommerce_before_single_product', 'astra_child_custom_price_display', 1);
+
+/**
+ * Display variant price
+ */
+function astra_child_display_variant_price() {
+    if (!isset($_GET['mg_type']) || !class_exists('MG_Virtual_Variant_Manager')) {
+        return;
+    }
+    
+    global $product;
+    $requested_type = sanitize_text_field($_GET['mg_type']);
+    $config = MG_Virtual_Variant_Manager::get_frontend_config($product);
+    
+    if (empty($config) || empty($config['types']) || !isset($config['types'][$requested_type])) {
+        return;
+    }
+    
+    $type_data = $config['types'][$requested_type];
+    
+    // Get variant price
+    $variant_price = isset($type_data['price']) && $type_data['price'] > 0 
+        ? (float) $type_data['price'] 
+        : (float) $product->get_price();
+    
+    // Format and display
+    $currency_symbol = get_woocommerce_currency_symbol();
+    $formatted_price = number_format($variant_price, 0, '', ' ');
+    
+    echo '<p class="price">';
+    echo '<span class="woocommerce-Price-amount amount">';
+    echo '<bdi>' . $formatted_price . '&nbsp;<span class="woocommerce-Price-currencySymbol">' . $currency_symbol . '</span></bdi>';
+    echo '</span>';
+    echo '</p>';
+}
+
+/**
+ * Override product title for variants
+ */
+function astra_child_variant_title($title, $id = null) {
+    if (!is_product() || !in_the_loop() || !is_main_query()) {
+        return $title;
+    }
+    
+    if (!isset($_GET['mg_type']) || !class_exists('MG_Virtual_Variant_Manager')) {
+        return $title;
+    }
+    
+    global $product;
+    if (!$product) {
+        return $title;
+    }
+    
+    $requested_type = sanitize_text_field($_GET['mg_type']);
+    $config = MG_Virtual_Variant_Manager::get_frontend_config($product);
+    
+    if (empty($config) || empty($config['types']) || !isset($config['types'][$requested_type])) {
+        return $title;
+    }
+    
+    $type_data = $config['types'][$requested_type];
+    $type_label = isset($type_data['label']) ? $type_data['label'] : $requested_type;
+    
+    if (strpos($title, ' - ' . $type_label) === false) {
+        return $title . ' - ' . $type_label;
+    }
+    
+    return $title;
+}
+add_filter('the_title', 'astra_child_variant_title', 10, 2);
+
+/**
  * Theme setup
  */
 function astra_child_theme_setup() {
--- astra-child/woocommerce/single-product/price.php
+++ astra-child/woocommerce/single-product/price.php
@@ -16,19 +16,27 @@ if (!defined('ABSPATH')) {
 
 global $product;
 
+// DEBUG: Output HTML comment to verify template is loading
+echo '<!-- MG Custom Price Template Loading -->' . PHP_EOL;
+
 // Check if variant type is specified in URL
 if (isset($_GET['mg_type']) && class_exists('MG_Virtual_Variant_Manager')) {
     $requested_type = sanitize_text_field($_GET['mg_type']);
+    echo '<!-- MG: mg_type parameter detected: ' . esc_html($requested_type) . ' -->' . PHP_EOL;
+    
     $config = MG_Virtual_Variant_Manager::get_frontend_config($product);
     
     // If valid variant configuration exists
     if (!empty($config) && !empty($config['types']) && isset($config['types'][$requested_type])) {
+        echo '<!-- MG: Valid variant config found -->' . PHP_EOL;
         $type_data = $config['types'][$requested_type];
         
         // Get variant price (or fall back to product price)
         $variant_price = isset($type_data['price']) && $type_data['price'] > 0 
             ? (float) $type_data['price'] 
             : (float) $product->get_price();
+        
+        echo '<!-- MG: Variant price: ' . $variant_price . ' -->' . PHP_EOL;
         
         // Get currency symbol
         $currency_symbol = get_woocommerce_currency_symbol();
@@ -45,10 +53,17 @@ if (isset($_GET['mg_type']) && class_exists('MG_Virtual_Variant_Manager')) {
         </p>
         <?php
         
+        echo '<!-- MG: Variant price output complete -->' . PHP_EOL;
+        
         // Stop here - don't output default price
         return;
+    } else {
+        echo '<!-- MG: Config empty or type not found -->' . PHP_EOL;
     }
+} else {
+    echo '<!-- MG: mg_type NOT set or MG_Virtual_Variant_Manager class missing -->' . PHP_EOL;
 }
 
 // Default: Output standard WooCommerce price
+echo '<!-- MG: Outputting default WooCommerce price -->' . PHP_EOL;
 echo $product->get_price_html();
